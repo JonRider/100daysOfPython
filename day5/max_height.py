d7b1cb2439ea94a13c69619f0fe3584b29a6c8c4
JonRider <jon@rider.net>
2021-08-03 08:25:25 +0530

    day5 partial

---
+++ day5/max_height.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+# Write your code below this row 👇
+max = 0
+for height in student_heights:
+    if height > max:
+        max = height
+print(max)

ccc4016977d85775c3c5f4ea92eb95bd6c781ea0
JonRider <jon@rider.net>
2021-08-06 11:11:57 +0530

    Day 8 begun

---
+++ day8/functions.py
@@ -0,0 +1,18 @@
+# def greet():
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
+#
+# greet()
+#
+# def greet_with_name(name, location):
+#     print(f"Hello {name}")
+#     print(f"How is the weather in {location}")
+#
+# greet_with_name("Jonathan", "Varanasi")
+
+def greet_with_name(name, location):
+    print(f"Hello {name}")
+    print(f"How is the weather in {location}")
+
+greet_with_name(location="India", name="Jon")

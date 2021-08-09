e4cbdb8381048362c48b758fa06358a4ca1dc0b1
JonRider <jon@rider.net>
2021-08-09 08:38:52 +0530

    Day 10 partial

---
+++ day10/output.py
@@ -0,0 +1,8 @@
+# Functions with names
+
+def my_function(first, last):
+    f_name = first.title()
+    l_name = last.title()
+    return f_name + " " + l_name
+
+print(my_function("jonathan", "rider"))

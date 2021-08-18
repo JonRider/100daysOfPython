432296cc1e195e9e7a6fecc5a50597f712f2aee0
JonRider <jon@rider.net>
2021-08-18 08:40:40 +0530

    Day 14 completed

--- day14/main.py
+++ day14/main.py
@@ -6,45 +6,50 @@ from os import system
 
 def print_vs(A, B):
     """Formats and prints A vs B"""
-    print(
-        f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
+    print(f"Compare A: {format_data(A)}")
     print(vs)
-    print(
-        f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
+    print(f"Against B: {format_data(B)}")
 
 
-def compare_followers(A, B):
-    """Compare number of social media followes. Returns 'a' or 'b'"""
-    if A['follower_count'] > B['follower_count']:
-        return 'a'
+def format_data(account):
+    return f"{account['name']}, a {account['description']}, from {account['country']}."
+
+
+def check_answer(a_followers, b_followers, choice):
+    """Checks which answer is right and returns true if user is correct"""
+    if a_followers > b_followers:
+        return choice == 'a'
     else:
-        return 'b'
+        return choice == 'b'
 
 
 # Start Game
 print(logo)
 score = 0
 A = random.choice(data)
-right = True
-while right:
+is_right = True
+while is_right:
     B = random.choice(data)
     while A == B:
         B = random.choice(data)
-    print_vs(A, B)
 
-    # Compare followers
-    high = compare_followers(A, B)
+    # Print VS Text
+    print_vs(A, B)
 
     # Get Choice
     choice = input("Who has more followers? Type 'A' or 'B': ").lower()
 
+    # Compare followers and check answer
+    a_followers = A['follower_count']
+    b_followers = B['follower_count']
+    is_right = check_answer(a_followers, b_followers, choice)
+
     # Check answer
-    if choice == high:
+    if is_right:
         score += 1
         system("clear")
+        print(logo)
         print(f"You're right! Current score: {score}")
-        if high == 'b':
-            A = B
+        A = B
     else:
         print(f"Sorry, that's wrong. Final score: {score}")
-        right = False
